3f6272a4fc21a8faa542de912c87682ea328643a
DevikaI <devika_indla@hotmail.com>
2014-02-12 15:58:54 +1000

    Update resources.groovy

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -2,7 +2,7 @@ import au.com.redboxresearchdata.curationmanager.identityProviderService.Curatio
 import au.com.redboxresearchdata.curationmanager.utility.ApplicationContextHolder;
 
 beans = {
-	//vndJsonErrorRenderer(grails.rest.render.errors.VndErrorJsonRenderer)
+	vndJsonErrorRenderer(grails.rest.render.errors.VndErrorJsonRenderer)
 	importBeans('file:grails-app/conf/spring/identityProviderServiceApplicationContext.xml')
 	applicationContextHolder(ApplicationContextHolder) { bean ->
 		bean.factoryMethod = 'getInstance'
