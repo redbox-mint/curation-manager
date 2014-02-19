2b59af33ae51408aea02904184276ba7c304dc24
DevikaI <devika_indla@hotmail.com>
2014-02-19 13:50:24 +1000

    Update CurationJob.groovy

--- grails-app/domain/au/com/redboxresearchdata/curationmanager/domain/CurationJob.groovy
+++ grails-app/domain/au/com/redboxresearchdata/curationmanager/domain/CurationJob.groovy
@@ -1,28 +1,3 @@
-//package curationmanagernew
-//
-//class CurationJob{
-//
-//    static constraints = {
-//		dateCompleted blank: true, nullable: true;
-//    }
-//	//List<CurationJobItems> curationJobItems;
-//	Date dateCreated;
-//	Date dateCompleted
-//	CurationStatusLookup curationStatusLookup;
-//
-//	static hasMany = [curationJobItems: CurationJobItems]
-//
-//	//static fetchMode = [curationJobItems: 'eager']
-//
-//	static mapping ={
-//		 id column : 'job_id'
-//		 curationStatusLookup column : 'status_id'
-//		// curationJobItems lazy: false
-//		 curationJobItems fetch:'join'
-//		// curationJobItems joinTable:[name:'curation_job_items', column:'job_id']
-//	}
-//}
-
 package au.com.redboxresearchdata.curationmanager.domain
 
 class CurationJob {
