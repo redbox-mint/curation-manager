99cfa42c72819059fc124c0b82b86f486c4922e4
DevikaI <devika_indla@hotmail.com>
2014-04-03 20:51:14 +1000

    Update ApplicationContextHolder.groovy

--- src/groovy/au/com/redboxresearchdata/curationmanager/utility/ApplicationContextHolder.groovy
+++ src/groovy/au/com/redboxresearchdata/curationmanager/utility/ApplicationContextHolder.groovy
@@ -1,48 +1,44 @@
 package au.com.redboxresearchdata.curationmanager.utility
 
-import javax.servlet.ServletContext
 import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.codehaus.groovy.grails.plugins.GrailsPluginManager
 import org.codehaus.groovy.grails.context.support.PluginAwareResourceBundleMessageSource
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
+
+import grails.util.Holders
  
 @Singleton
 class ApplicationContextHolder implements ApplicationContextAware {
  
-   private ApplicationContext ctx
-   
-   private static final Map<String, Object> TEST_BEANS = [:]
- 
-   void setApplicationContext(ApplicationContext applicationContext) {
-       ctx = applicationContext
-   }
- 
-   static ApplicationContext getApplicationContext() {
-      getInstance().ctx
+  static  ApplicationContext getApplicationContext() {
+        Holders.grailsApplication.mainContext
    }
    
    static GrailsApplication getGrailsApplication() {
-	   getBean('grailsApplication')
-	}
-  
-	static ConfigObject getConfig() {
-	   getGrailsApplication().config
+		ApplicationContext ctx = Holders.grailsApplication.mainContext
+		ctx.getBean('grailsApplication')
 	}
   
 	static ServletContext getServletContext() {
-	   getBean('servletContext')
+		 ApplicationContext ctx = Holders.grailsApplication.mainContext
+		 ctx.getBean('servletContext')
 	}
-  
-	static GrailsPluginManager getPluginManager() {
-	   getBean('pluginManager')
+   static GrailsPluginManager getPluginManager() {
+	   ApplicationContext ctx = Holders.grailsApplication.mainContext
+	   ctx.getBean('pluginManager')
 	}
 	
-	static Object getBean(String name) {
-		TEST_BEANS[name] ?: getApplicationContext().getBean(name)
-	 }
 	
 	static PluginAwareResourceBundleMessageSource getMessageSource() {
-	   getBean("messageSource");
+		ApplicationContext ctx = Holders.grailsApplication.mainContext
+	    ctx.getBean("messageSource");
 	} 
+
+	@Override
+	public void setApplicationContext(ApplicationContext ctx)
+			throws BeansException {
+		
+	}
 }
