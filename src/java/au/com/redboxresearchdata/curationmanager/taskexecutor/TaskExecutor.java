25c87fc7cd194c1b8bfc8b5889fb1efef67a3cd6
Shilo Banihit <shilo@redboxresearchdata.com.au>
2014-04-03 12:25:04 +1000

    REDBOX-219 #COMMENT Removing some beans from xml context.

--- src/java/au/com/redboxresearchdata/curationmanager/taskexecutor/TaskExecutor.java
+++ src/java/au/com/redboxresearchdata/curationmanager/taskexecutor/TaskExecutor.java
@@ -2,19 +2,18 @@ package au.com.redboxresearchdata.curationmanager.taskexecutor;
 
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
-
-import org.springframework.context.ApplicationContext;
-
-import au.com.redboxresearchdata.curationmanager.utility.ApplicationContextHolder;
-
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 public class TaskExecutor {
 	private static final Log log = LogFactory.getLog(TaskExecutor.class);
 	
     public TaskExecutor(){
     	try{
-    	  ApplicationContext ctx = ApplicationContextHolder.getApplicationContext();
-    	  CurationTaskExecutor myTaskExecutor = (CurationTaskExecutor) ctx.getBean("curationTaskExecutor");
+    	  ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+    	  taskExecutor.setCorePoolSize(1);
+    	  taskExecutor.setMaxPoolSize(1);
+    	  taskExecutor.setQueueCapacity(1);    	        	  
+    	  CurationTaskExecutor myTaskExecutor = new CurationTaskExecutor(taskExecutor);
     	  myTaskExecutor.executeTask();
     	}catch(Exception ex){
     		log.error(ex.getMessage());
