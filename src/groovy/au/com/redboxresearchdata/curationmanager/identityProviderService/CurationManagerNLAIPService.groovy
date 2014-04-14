849b578f5ab89c0740794ad5b3e87e12829d597e
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2014-04-14 11:02:27 +0930

    Changed name of method that produces message for the Harvest Manager to be more descriptive. Fixed output of created message to match requirements.

--- src/groovy/au/com/redboxresearchdata/curationmanager/identityProviderService/CurationManagerNLAIPService.groovy
+++ src/groovy/au/com/redboxresearchdata/curationmanager/identityProviderService/CurationManagerNLAIPService.groovy
@@ -26,45 +26,45 @@ import au.com.redboxresearchdata.curationmanager.identityProviderService.utility
 import au.com.redboxresearchdata.curationmanager.identityProviderService.utility.MessageResolver;
 
 class CurationManagerNLAIPService implements IdentityProviderService{
-	
+
 	private static final Log log = LogFactory.getLog(this)
 
 	@Value("#{nlaPropSource[Id]}")
 	String id;
-	
+
 	@Value("#{nlaPropSource[Name]}")
 	String name;
-	
+
 	@Value("#{nlaPropSource[Type]}")
 	String type;
-	
+
 	@Value("#{nlaPropSource[Template]}")
 	String template;
-	
+
 	@Value("#{nlaPropSource[Agent]}")
 	String agent
-	
+
 	@Value("#{nlaPropSource[AgencyCode]}")
 	String agencyCode;
-	
+
 	@Value("#{nlaPropSource[AgencyName]}")
 	String agencyName;
-	
+
 	@Value("#{nlaPropSource[MetadataPrefix]}")
 	String metadataPrefix;
-	
+
 	@Value("#{nlaPropSource[RecordSource]}")
 	String recordSource;
-	
+
 	@Value("#{nlaPropSource[DependentIdentityProviderName]}")
 	String dependentIdentityProviderName;
-	
-	
+
+
 	private Boolean isSynchronous = Boolean.FALSE;
-	 
+
 	private Boolean exists;
-	
-    @Override
+
+	@Override
 	public String getId() {
 		return id;
 	}
@@ -73,57 +73,57 @@ class CurationManagerNLAIPService implements IdentityProviderService{
 	public String getName() {
 		return name;
 	}
-	
+
 	@Override
 	public String getType() {
 		return  type;
 	}
-	
-	
+
+
 	@Override
 	public String getAgent() {
 		return agent;
 	}
-	
+
 	@Override
 	public String getRecordSource() {
 		return recordSource;
 	}
-	
+
 	@Override
 	public String[] getMetadataPrefix() {
-		 return metadataPrefix.split(",");
+		return metadataPrefix.split(",");
 	}
-	
+
 	public Map<String, String> getMetaDataMap(String metaData) {
 		return Boolean.TRUE;
 	}
-	
+
 	public Boolean validate(Map.Entry<String, String> pairs, String requestType) throws Exception{
-	   if(null!= type && !type.contains(requestType)){ 
-		  def msg = MessageResolver.getMessage(IdentityServiceProviderConstants.IDENTITY_SERVICE_TYPE_NLA_DOES_NOT_MATCH);
-		  log.error(msg + " " + requestType);
-	 	  throw new CurationManagerBSException(IdentityServiceProviderConstants.STATUS_400, msg+ " " + requestType);
+		if(null!= type && !type.contains(requestType)){
+			def msg = MessageResolver.getMessage(IdentityServiceProviderConstants.IDENTITY_SERVICE_TYPE_NLA_DOES_NOT_MATCH);
+			log.error(msg + " " + requestType);
+			throw new CurationManagerBSException(IdentityServiceProviderConstants.STATUS_400, msg+ " " + requestType);
 		}
 		IdentityProviderService  dependentIdtityPrviderService = getDependentProviderService();
 		dependentIdtityPrviderService.validate(pairs, requestType);
-	    NLAValidator nlaValidator = new NLAValidator();
-	    return nlaValidator.validateMetaData(pairs);
+		NLAValidator nlaValidator = new NLAValidator();
+		return nlaValidator.validateMetaData(pairs);
 	}
-	
+
 	public String getDependentIdentityProviderName(){
 		return dependentIdentityProviderName;
 	}
-	
+
 	public IdentityProviderService getDependentProviderService() throws Exception{
-	  if(null!= getDependentIdentityProviderName()) {
-		 ApplicationContext applicationContext =  ApplicationContextHolder.getApplicationContext();
-		 return applicationContext.getBean(getDependentIdentityProviderName());
-	  } else if(null == getDependentIdentityProviderName()){
-		log.error("No Dependent Identity Provider Service configured for NLA ");
-	    throw new Exception(IdentityServiceProviderConstants.STATUS_400, 
-				  "No Dependent Identity Provider Service configured for NLA");
-	  }
+		if(null!= getDependentIdentityProviderName()) {
+			ApplicationContext applicationContext =  ApplicationContextHolder.getApplicationContext();
+			return applicationContext.getBean(getDependentIdentityProviderName());
+		} else if(null == getDependentIdentityProviderName()){
+			log.error("No Dependent Identity Provider Service configured for NLA ");
+			throw new Exception(IdentityServiceProviderConstants.STATUS_400,
+			"No Dependent Identity Provider Service configured for NLA");
+		}
 	}
 
 	@Override
@@ -134,106 +134,113 @@ class CurationManagerNLAIPService implements IdentityProviderService{
 	public String getTemplate(){
 		return template;
 	}
-	
+
 	public String getAgencyCode(){
 		return agencyCode;
 	}
-	
+
 	public String getAgencyName(){
 		return agencyName;
 	}
-	
+
 	@Override
-	public BaseIdentityResult curate(String oid, String... metaData) throws Exception {	 
-    if(null!= getDependentProviderService() && null == metaData[3]){
-		log.error("No Dependent Identifier");
-        throw new Exception("No Dependent Identifier");
-    }
-	 BaseIdentityResult baseIdentityResult = null;
-	 try{	
-	   ApplicationContext applicationContext =  ApplicationContextHolder.getApplicationContext();
-	   Map metaDataMap = mapFromMetaData(oid, metaData[0], metaData[1], metaData[2], metaData[3]);	 
-	   JSON jsonMessage = map2Json(metaDataMap)
-	   String validJson = jsonMessage.toString();
-	   def jmsService = applicationContext.getBean(IdentityServiceProviderConstants.JMS_SERVICE);
-	   jmsService.send(queue:"oaiPmhFeed", validJson);
-	   baseIdentityResult = new PromiseResult()
-	   baseIdentityResult.setIdentifier(oid);
-	}catch(Exception ex){
-	  log.error(ex.getMessage()+ "NLA Identy provider failed" + ex.getCause());
-	  throw new Exception("NLA Identy provider failed");
+	public BaseIdentityResult curate(String oid, String... metaData) throws Exception {
+		if(null!= getDependentProviderService() && null == metaData[3]){
+			log.error("No Dependent Identifier");
+			throw new Exception("No Dependent Identifier");
+		}
+		BaseIdentityResult baseIdentityResult = null;
+		try{
+			ApplicationContext applicationContext =  ApplicationContextHolder.getApplicationContext();
+			Map metaDataMap = makeHarvestManagerMessage(oid, metaData[0], metaData[1], metaData[2], metaData[3]);
+			JSON jsonMessage = map2Json(metaDataMap)
+			String validJson = jsonMessage.toString();
+			def jmsService = applicationContext.getBean(IdentityServiceProviderConstants.JMS_SERVICE);
+			jmsService.send(queue:"oaiPmhFeed", validJson);
+			baseIdentityResult = new PromiseResult()
+			baseIdentityResult.setIdentifier(oid);
+		}catch(Exception ex){
+			log.error(ex.getMessage()+ "NLA Identy provider failed" + ex.getCause());
+			throw new Exception("NLA Identy provider failed");
+		}
+		return baseIdentityResult;
 	}
-	  return baseIdentityResult;
-	} 
-	
+
 	private JSON map2Json(Map metaDataMap) throws Exception{
 		JSON json = metaDataMap as JSON
 		if(null != json){
-		  return json;
+			return json;
 		}
 		return null;
 	}
-	
-	private Map mapFromMetaData(String oid, String metaData, String jobId, String type, String depdentIdentifier)
-	  throws Exception{	 	  
-	  Map<String, String> event = new HashMap();
-	  Map requestJsonMap = JsonUtil.getMapFromMetaDataForNLA(metaData);
-	  event.put("eventDateTime_standardDateTime", DateUtil.getW3CDate());
-	  event.put("agent", getAgent());
-	  	  
-	  Map maintenanceEvent = new HashMap();
-	  maintenanceEvent.put("maintenenceEvent", event);
-	  
-	  Map<String, String> agency = new HashMap<String, String>();
-	  agency.put("agencyCode", getAgencyCode());
-	  agency.put("agencyName", getAgencyName());
-	  
-	  Map<String, Map> maintenanceAgency = new HashMap();
-	  maintenanceAgency.put("maintenanceAgency", agency);
-	  maintenanceAgency.put("maintenanceHistory", maintenanceEvent);
-	  
-	  Map<String, String> recordId = new HashMap();
-	  IdentityProviderIncrementor localIdentityProviderIncrementor = new IdentityProviderIncrementor()
-	  localIdentityProviderIncrementor.save();
-	  String idIncrementor;
-	  if(null != localIdentityProviderIncrementor && null != localIdentityProviderIncrementor.id){
-		  idIncrementor = localIdentityProviderIncrementor.id.toString();
-		  recordId.put("recordId", jobId + oid + idIncrementor);
-	  }else{
-	     recordId.put("recordId", jobId + oid);
-	  }
-	  recordId.put("metadataPrefix", getMetadataPrefix());
-	  recordId.put("source", getRecordSource()); 
-	  
-	  Map jsonData = new HashMap();
-	  jsonData.put("recordId", oid);
-	  jsonData.put("control", maintenanceAgency);
-	  jsonData.put("entityId", depdentIdentifier);
-	  jsonData.put("surname", requestJsonMap.get("surname"));
-	  jsonData.put("forename", requestJsonMap.get("forename"));
-	  jsonData.put("description", requestJsonMap.get("description"));
-	  jsonData.put("salutation", requestJsonMap.get("salutation"));
-	  jsonData.put("dateStamp", DateUtil.getW3CDate());
-	   
-	  recordId.put("jsonData", jsonData);
-	  
-	  Map<String, Map> data = new HashMap();
-	  data.put("data", recordId);
 
-	  Map<String, Map> typeRecord = new TreeMap();
-	  String record = "record";
-	  if(null != getType()){
-		 record =  "record"+"_"+getType()
-	  }
-	  typeRecord.put("type", record);	
-	 
-	  Map<String, Map> header = new TreeMap<String, Map>();
-	  header.put("header",  typeRecord);
-	  header.put("data",  recordId);
+	/**
+	 * Produce a message for the Harvest Manager so that a OAI-PMH record can be created for the NLA to harvest
+	 * 
+	 * @param oid
+	 * @param metaData
+	 * @param jobId
+	 * @param type
+	 * @param depdentIdentifier
+	 * @return
+	 * @throws Exception
+	 */
+	private Map makeHarvestManagerMessage(String oid, String metaData, String jobId, String type, String depdentIdentifier)
+	throws Exception{
+		//TODO: Remove this line as it shouldn't filter anything that comes from ReDBox/Mint
+		Map requestJsonMap = JsonUtil.getMapFromMetaDataForNLA(metaData);
+		def recordSource = "Curation Manager NLA Identity Provider"
+		IdentityProviderIncrementor localIdentityProviderIncrementor = new IdentityProviderIncrementor()
+		localIdentityProviderIncrementor.save();
+		String idIncrementor;
+		//TODO: Investigate the need for this
+		def recordId = ""
+		if(null != localIdentityProviderIncrementor && null != localIdentityProviderIncrementor.id){
+			idIncrementor = localIdentityProviderIncrementor.id.toString();
+			recordId = jobId + oid + idIncrementor;
+		}else{
+			recordId =  jobId + oid;
+		}
 
-	  return header;
+		def mdPrefix = ["eac-cpf", "oai_dc"]
+
+		//Build the record template data
+		def jsonDataMap = [
+			"recordId":oid,
+			"control":[
+				"maintenanceAgency":["agencyCode":getAgencyCode(), "agencyName":getAgencyName()],
+				"maintenanceHistory":[
+					"maintenanceEvent":[
+						"eventDateTime_standardDateTime":DateUtil.getW3CDate(),
+						"agent":getAgent()
+					]
+				]
+			],
+			"entityId":depdentIdentifier,
+			"dateStamp":DateUtil.getW3CDate()
+		]
+		
+		//Merge in all the metadata we received from ReDBox/Mint about the record so that it can be used to generate the eac-cpf
+		jsonDataMap.putAll(requestJsonMap)
+
+		def jsonMapData = [
+			"header":[
+				"type":"record_person"
+			],
+			"data":[
+				[
+					"recordId":recordId,
+					"metadataPrefix":mdPrefix,
+					"source":recordSource,
+					"jsonData":jsonDataMap
+				]
+			]
+		]
+
+		return jsonMapData
 	}
-	
+
+
 	@Override
 	public Boolean exists(String oid, String[] metaData) {
 		return exists;
